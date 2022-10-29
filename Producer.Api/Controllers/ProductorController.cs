7fe5d44292676143a481c7125b05683d07927422
Andersson Oliveira <enigma601sp@hotmail.com>
2022-10-28 22:12:27 -0300

    Use kafka with ASPNET

---
+++ Producer.Api/Controllers/ProductorController.cs
@@ -0,0 +1,29 @@
+﻿using application.Entities;
+using application.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Producer.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductorController : ControllerBase
+    {
+
+        private readonly ILogger<ProductorController> _logger;
+        private readonly IProductService _service;
+
+        public ProductorController(ILogger<ProductorController> logger, IProductService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<int> PostAsync([FromBody] Product resquest)
+        {
+            return await _service.GenerateAsync(resquest);
+        }
+    }
+}
---
+++ application/Service/ProductService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using application.Entities;
+using application.Interface;
+using application.Infra;
+using Confluent.Kafka;
+
+namespace application.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly string _host = EnvConfig.KafkaHost();
+        public ProductService()
+        {
+            
+        }
+
+        public async Task<int> GenerateAsync(Product request)
+        {
+            string bootstrapServers = _host;
+            string nomeTopic = "Product";
+            try
+            {
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = bootstrapServers
+                };
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+
+                        var result = await producer.ProduceAsync(
+                            nomeTopic,
+                            new Message<Null, string>
+                            { Value = request.Description });
+                }
+
+                return 200;
+            }
+            catch (Exception)
+            {
+
+                return 404;
+            }
+        }
+
+        public void Receive()
+        {           
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+        }
+    }
+}
